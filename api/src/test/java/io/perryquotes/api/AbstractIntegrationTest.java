2f0d36570ad8c83e697aa54c6f92604b9b494fe9
Markus Lamm <markus.lamm@gmail.com>
2021-07-04 13:07:26 +0200

    refactor Testcontainer confi

---
+++ api/src/test/java/io/perryquotes/api/AbstractIntegrationTest.java
@@ -0,0 +1,23 @@
+package io.perryquotes.api;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public abstract class AbstractIntegrationTest {
+
+  private final static String POSTGRES_DOCKER_IMAGE = "postgres:13.2-alpine";
+
+  static final PostgreSQLContainer<?> CONTAINER = new PostgreSQLContainer<>(POSTGRES_DOCKER_IMAGE);
+
+  static {
+    CONTAINER.start();
+  }
+
+  @DynamicPropertySource
+  static void postgresProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", CONTAINER::getJdbcUrl);
+    registry.add("spring.datasource.username", CONTAINER::getUsername);
+    registry.add("spring.datasource.password", CONTAINER::getPassword);
+  }
+}
--- api/src/test/java/io/perryquotes/api/ApiApplicationTests.java
+++ api/src/test/java/io/perryquotes/api/ApiApplicationTests.java
@@ -1,12 +1,13 @@
 package io.perryquotes.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
-class ApiApplicationTests {
+//@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class ApiApplicationTests extends AbstractIntegrationTest {
 
   @Test
   void contextLoads() {
