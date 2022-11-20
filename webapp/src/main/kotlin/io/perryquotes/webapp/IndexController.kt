d36fc00fdfc24b48a863c401674499ce84fdb866
Markus Lamm <markus.lamm@gmail.com>
2022-11-20 18:20:57 +0100

    very basic index page

---
+++ webapp/src/main/kotlin/io/perryquotes/webapp/IndexController.kt
@@ -0,0 +1,13 @@
+package io.perryquotes.webapp
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class IndexController {
+
+    @GetMapping
+    fun index(): String {
+        return "index"
+    }
+}
