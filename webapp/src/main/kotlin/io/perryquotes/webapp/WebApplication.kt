521419229e687b2122b1127d24d4fd013e33632e
Markus Lamm <markus.lamm@gmail.com>
2022-11-20 18:05:01 +0100

    first api configuration via application.properties

--- webapp/src/main/kotlin/io/perryquotes/webapp/WebApplication.kt
+++ webapp/src/main/kotlin/io/perryquotes/webapp/WebApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Application
+class WebApplication
 
 fun main(args: Array<String>) {
-	runApplication<Application>(*args)
+	runApplication<WebApplication>(*args)
 }
